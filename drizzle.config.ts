7378114fe2a76a77875bd05e3bf8646459f75908
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2026-05-09 12:23:14 -0500

    fix(config): update environment variable to SWAP_DATABASE_URL

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not set in .env');
+if (!process.env.SWAP_DATABASE_URL) {
+  throw new Error('SWAP_DATABASE_URL is not set in .env');
 }
 
 module.exports = {
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: process.env.SWAP_DATABASE_URL,
   },
   tablesFilter: ['front-end_*'],
 };
